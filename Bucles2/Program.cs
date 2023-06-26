24d9426ee836ad22a94a3e66d72ec111cde5e5a7
LeonelGirett <82616033+leonel1414@users.noreply.github.com>
2023-06-26 20:56:56 -0300

    Realizando ejercicios de pracica

--- Bucles2/Program.cs
+++ Bucles2/Program.cs
@@ -81,3 +81,61 @@ for (int z =0; z <10; z++) {
     }
 }
 */
+//Ejercicio 1 - While
+
+//Escribe una tabla de multiplicar del 1 al 10 para un número entero que recibe por consola. Es decir, un programa que presente para el 1:
+
+/*
+1 x 1 = 1
+1 x 2 = 2
+…
+1 x 10 = 10
+
+Console.WriteLine("Ingrese un numero a multiplicar");
+int numero = Convert.ToInt32(Console.ReadLine());
+int contador = 0;
+
+while (contador <10) {
+    contador++;
+     int resultado = contador * numero;
+    Console.WriteLine(numero + " x "+ contador + " = "+ resultado);
+}
+*/
+
+
+//Ejercicio 2 - Do while
+//Escribe un programa que realice estos pasos:
+//Reciba al menos un número por consola
+//Determine si el número es positivo o negativo
+//Presente un contador para cada tipo (positivo y negativo).
+//Nota: el cero se puede abordar en una clase adicional ya que no es ni positivo ni negativo. Tienes completa libertad para elegir el formato de la salida.
+
+
+int contador_positivo = 0;
+int contador_negativo = 0;
+int numero;
+do {
+    Console.WriteLine("Ingrese un numero");
+    numero = Convert.ToInt32(Console.ReadLine());
+
+    if (numero > 0)
+    {
+        Console.WriteLine("El numero es positivo");
+        contador_positivo++;
+    }
+    else if (numero < 0)
+    {
+        Console.WriteLine("El numero es NEGATIVO");
+        contador_negativo++;
+    }
+    else {
+        Console.WriteLine("El numero 0 es neutro");
+    }
+
+}while (numero != 0);
+
+Console.WriteLine("El total de numero positivo es: " + contador_positivo);
+Console.WriteLine("El total de numero Negativo es: " + contador_negativo);
+
+
+
