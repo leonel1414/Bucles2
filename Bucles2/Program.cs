b34685c363eb1d281fc9954e11c4cb55472dd5ed
LeonelGirett <82616033+leonel1414@users.noreply.github.com>
2023-06-26 19:42:02 -0300

    Agregar archivos de proyecto.

---
+++ Bucles2/Program.cs
@@ -0,0 +1,73 @@
+﻿/*
+//Bucles
+
+//While = mientras
+
+int i = 0;
+
+Console.Write("Ciclo WHILE: ");
+while (i < 10)
+{
+    Console.Write(i);
+    i++;
+}
+
+
+//Do.. while = hacer..mientras
+Console.Write("\nCiclo DO WHILE: ");
+int k = 5;
+do
+{
+    Console.Write(k + " ");
+    k++;
+} while (k < 10);
+
+//FOR = para
+Console.Write("\nCiclo FOR: ");
+for (int j = 0; j <= 10; j++)
+{
+    Console.Write(" " + j);
+}
+
+//Foreach
+Console.Write("\n");
+var names = new List<string> { "leonel", "belen", "milena" };
+foreach (var name in names)
+{
+    Console.WriteLine("Hola, " + name.ToUpper());
+}
+
+//BUCLES 2
+
+// IF
+Console.WriteLine("¿Que dia es hoy?");
+string hoy = Console.ReadLine();
+if (hoy == "Lunes")
+{
+    Console.WriteLine("Feliz Lunes");
+}
+else
+{
+    Console.WriteLine("Hoy no es Lunes");
+}
+*/
+
+//SWITCH
+
+Console.WriteLine("¿Que dia es hoy?");
+string hoy = Console.ReadLine();
+
+switch (hoy) { 
+    case "lunes":
+        Console.WriteLine("Hoy es lunes");
+        break;
+    case "martes":
+        Console.WriteLine("Hoy es martes");
+        break;
+    case "miercoles":
+        Console.WriteLine("Hoy es miercoles");
+        break;
+    default:
+        Console.WriteLine("Ese dia no existe");
+        break;
+}
