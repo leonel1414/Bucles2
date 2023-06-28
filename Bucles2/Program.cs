c8612348f39a1d0e67e86f887bc57a8c17619ad3
LeonelGirett <82616033+leonel1414@users.noreply.github.com>
2023-06-28 19:00:52 -0300

    Relizando ejercicios de practica2

--- Bucles2/Program.cs
+++ Bucles2/Program.cs
@@ -102,7 +102,8 @@ while (contador <10) {
 }
 */
 
-
+//***************************************************************//
+/*
 //Ejercicio 2 - Do while
 //Escribe un programa que realice estos pasos:
 //Reciba al menos un número por consola
@@ -136,6 +137,72 @@ do {
 
 Console.WriteLine("El total de numero positivo es: " + contador_positivo);
 Console.WriteLine("El total de numero Negativo es: " + contador_negativo);
+*/
+
+//Ejercicio 1 - If
+//Escribe un programa que:
+//Pida datos a un cliente: Nombre, email, cupón
+//Determine si un cliente tiene un cupon descuento
+//Muestre un precio rebajado en función del descuento
+//Muestre el precio de un producto sin descuento si no hay cupón
+//Nota: puedes poner un precio fijo de un producto o uno variable.
+
+//******************************************************************//
 
 
+/*
+//Datos de un cliente
+int cupon;
+double precio_rebajado;
+int precio = 1000;
+double total;
+Console.WriteLine("Escriba un nombre:");
+string nombre = Console.ReadLine();
+
+Console.WriteLine("Escriba un mail");
+string mail = Console.ReadLine();
+
+Console.WriteLine("Escriba el descuento");
+cupon = Convert.ToInt32(Console.ReadLine());
+
+if (cupon == 20)
+{
+    precio_rebajado = (precio * cupon)/100;
+    total= precio - precio_rebajado;
+    Console.WriteLine("***Ticket***");
+    Console.WriteLine("Nombre: "+ nombre);
+    Console.WriteLine("Mail: " + mail);
+    Console.WriteLine("Total: "+ total);
+}
+else{
+    Console.WriteLine("NO HAY DESCUENTO");
+    Console.WriteLine("Precio del PRODUCTO: " + precio);
+}
+*/
+
+//Ejercicio 2 - Switch
+//Haz una lista de lenguajes de programación, por ejemplo: C#, Java, C++. Presenta la lista en consola y pide que el usuario seleccione el lenguaje mediante 1, 2, 3 o a, b, c.
+//Presenta el resultado en consola.
+//Nota: puedes añadir al resultado el “Hola, mundo” para el caso de C#.
+
+Console.WriteLine("Ingrese el lenguaje que le gusta:");
+Console.WriteLine("a. Java");
+Console.WriteLine("b. Python");
+Console.WriteLine("c. Php");
+Console.WriteLine("d. C#");
+string opcion = Console.ReadLine();
+switch (opcion) {
+    case "a":
+        Console.WriteLine("Le gusta JAVA");
+        break;
+    case "b": Console.WriteLine("Le gusta Python");
+        break;
+    case "c": Console.WriteLine("Le gusta PHP");
+        break;
+    case "d": Console.WriteLine("Le gusta C#");
+        break;
+    default:
+        Console.WriteLine("No eligio ninguna de las opciones");
+        break;
+}
 
